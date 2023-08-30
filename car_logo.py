2cab9ff44e3fff18d52478eb2640a9bccd02c2ca
kouchakun <seta.adachi@gmail.com>
2023-08-30 19:47:17 +0900

    app.run_change

--- car_logo.py
+++ car_logo.py
@@ -51,6 +51,6 @@ def upload_file():
 
 
 if __name__ == "__main__":
-    #port = int(os.environ.get('PORT', 8080))
-    #app.run(host ='0.0.0.0',port = port)
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
+    #app.run(debug=True)
